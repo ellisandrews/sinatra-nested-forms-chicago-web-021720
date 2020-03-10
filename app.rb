1dd947601225bd4468709a5dc4278a41eb014b94
ellisandrews <ellisandrews1@gmail.com>
2020-03-10 14:55:36 -0500

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,40 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :"pirates/new"
+    end
+
+    post '/pirates' do
+      
+      # Extract the data from the extraneous top-level key
+      pirate_data = params[:pirate]
+
+      # Make the Pirate object from the form data
+      @pirate = Pirate.new(
+        name: pirate_data[:name], 
+        weight: pirate_data[:weight],
+        height: pirate_data[:height],
+      )
+      
+      @ship_1 = Ship.new(
+        name: pirate_data[:ships][0][:name],
+        type: pirate_data[:ships][0][:type],
+        booty: pirate_data[:ships][0][:booty]
+      )
+
+      @ship_2 = Ship.new(
+        name: pirate_data[:ships][1][:name],
+        type: pirate_data[:ships][1][:type],
+        booty: pirate_data[:ships][1][:booty]
+      )
+      
+      erb :"pirates/show"
+    end
 
   end
 end
